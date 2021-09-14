a37567921fe801ceb2e35ffd6fc64b0129634845
ceduardo358 <72917759+ceduardo358@users.noreply.github.com>
2021-09-14 13:28:57 -0300

    atualizando scripts

---
+++ site_pizzaria/json.js
@@ -0,0 +1,12 @@
+
+let objpizzas = [
+    {id:1, name: "Mussarela", img:'imagens/pizza_mussarela.jpg', price:20.59, sizes:['100g', '500g', '800g']},
+    {id:2, name: "Calabreza", img:'imagens/pizza_calabresa.jpg', price:20.59, sizes:['100g', '500g', '800g']},
+    {id:3, name: "Doce", img:'imagens/pizza_doce.jpg', price:20.59, sizes:['100g', '500g', '800g']},
+    {id:4, name: "Frango", img:'imagens/pizza_frango.jpg', price:20.59, sizes:['100g', '500g', '800g']},
+    {id:5, name: "Frango com catupiry", img:'imagens/pizza_frango_catupiry.jpg', price:20.59, sizes:['100g', '500g', '800g']},
+    {id:6, name: "Marguerita", img:'imagens/pizza_marguerita.jpg', price:20.59, sizes:['100g', '500g', '800g']},
+    {id:7, name: "Portuguesa", img:'imagens/pizza_portuguesa.jpg', price:20.59, sizes:['100g', '500g', '800g']}
+]
+module.exports = objpizzas;
+
---
+++ site_pizzaria/script.js
@@ -0,0 +1,2 @@
+let obj = require('./json')
+console.log(obj)
